efe580b7e32ea519df3d0762ecf60fd92b9d1383
Uyirendiren Chemen <udchemen@gmail.com>
2020-06-13 10:10:29 -0400

    Working on a Tutorial for NavBar-24:26

---
+++ src/components/SideDrawer/SideDrawer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './SideDrawer.scss'
+
+const SideDrawer = props => (
+    <nav>
+        <ul>
+            <li><a href="/"></a>Products</li>
+            <li><a href="/"></a>Users</li>
+        </ul>
+    </nav>
+
+);
+
+export default SideDrawer;
