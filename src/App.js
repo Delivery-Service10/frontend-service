0e4e3fdd44c843860a8d2ca1ae5984a825d37232
Uyirendiren Chemen <udchemen@gmail.com>
2020-07-10 08:53:47 -0400

    Fresh React App

--- src/App.js
+++ src/App.js
@@ -1,72 +1,26 @@
-import React, {Component} from 'react';
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import Toolbar from './components/Toolbar/Toolbar'
-import SideDrawer from "./components/SideDrawer/SideDrawer";
-import Backdrop from "./components/Backdrop/Backdrop"
-import {Switch,Route, BrowserRouter, Redirect} from 'react-router-dom'
-import CreateAccountPage from "./components/pages/create-account/create-account.component";
-import LoginPage from "./components/pages/login/login.component";
-import HomePage from "./components/pages/homepage/homepage.component";
-import {connect} from 'react-redux';
 
-class App extends Component {
-    state = {
-        sideDrawerOpen: false
-
-    };
-  drawerToggleClickHandler = () =>{
-      this.setState((prevState) => {
-          return {sideDrawerOpen: !prevState.sideDrawerOpen}
-
-      })
-  };
-  backdropClickHandler = () =>{
-      this.setState({sideDrawerOpen:false});
-  };
-  render() {
-      const {currentUser} = this.props;
-      let backdrop;
-      if (this.state.sideDrawerOpen){
-          backdrop = <Backdrop click={this.backdropClickHandler}/>;
-      }
-    return (
-        <BrowserRouter>
-        <div style={{height: '100%'}}>
-          <Toolbar drawerClickHandler={this.drawerToggleClickHandler}
-                   currentUser={currentUser}
-          />
-            <SideDrawer show={this.state.sideDrawerOpen}/>
-            {backdrop}
-          <main style={{marginTop: '56px'}}>
-                  <Switch>
-                      <Route exact path='/' render={HomePage}/>
-                      <Route exact path='/login' render={
-                          ()=>this.props.currentUser?
-                              (<Redirect to='/'/>):
-                              (<LoginPage/>)
-                      }/>
-                      <Route exact path='/create-account' render={
-                          ()=>this.props.currentUser?
-                              (<Redirect to='/'/>):
-                              (<CreateAccountPage/>)
-                      }/>
-                  </Switch>
-
-          </main>
-        </div>
-        </BrowserRouter>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
-const mapStateToProps= ({user})=>({
-    currentUser: user.currentUser
-})
 
-const mapDispatchToProps = dispatch => ({
-    // setCurrentUser: user =>dispatch(setCurrentUser(user))
-});
-
-
-export default connect(mapStateToProps, mapDispatchToProps
-
-)(App);
+export default App;
